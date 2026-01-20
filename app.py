005656b5632ff97e3d2ee66a1531f0760f3208dc
Dildar Hussain <hussaindildar017@gmail.com>
2026-01-20 19:07:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import os
 import tempfile
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains import RetrievalQA
-from langchain_groq import ChatGroq
+
 
 # -----------------------------
 # Streamlit Page Config
